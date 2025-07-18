ccdc01d9e2da957de3915255ca2f252c03c9a801
Naylek_ <69121494+TTVNaylek@users.noreply.github.com>
2025-07-18 07:30:22 +0000

    eslintconfig update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,33 @@
-import { defineConfig } from "eslint/config";
-import stylistic from "@stylistic/eslint-plugin"
+import stylistic from "@stylistic/eslint-plugin";
+import {config} from "typescript-eslint";
+import parser from '@typescript-eslint/parser';
 
 
-export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: {
-      "@stylistic": stylistic
-    },
-    rules: {
-        "@stylistic/block-spacing": ["off"],
-        '@stylistic/brace-style': ["error", "1tbs", {"allowSingleLine": true}],
-        '@stylistic/indent': ["error", 4],
-        "@stylistic/multiline-ternary": ["off"],
-        "@stylistic/no-trailing-spaces": ["error", {"skipBlankLines": true}],
-        "@stylistic/object-curly-spacing": ["error", "never"],
-        '@stylistic/quotes': ["off"],
-        '@stylistic/semi': ["error", "always"],
-        "@stylistic/spaced-comment": ["off"],
+export default config([
+    {
+        files: ["**/*.{ts,mts,cts}"],
+        languageOptions: {
+            parser: parser,
+            parserOptions: {
+                project: './tsconfig.json',
+                sourceType: 'module',
+                ecmaVersion: 'latest',
+            }
+        },
+        plugins: {
+            '@typescript-eslint': typescriptPlugin,
+            "@stylistic": stylistic
+        },
+        rules: {
+            "@stylistic/block-spacing": ["off"],
+            '@stylistic/brace-style': ["error", "1tbs", {"allowSingleLine": true}],
+            '@stylistic/indent': ["error", 4],
+            "@stylistic/multiline-ternary": ["off"],
+            "@stylistic/no-trailing-spaces": ["error", {"skipBlankLines": true}],
+            "@stylistic/object-curly-spacing": ["error", "never"],
+            '@stylistic/quotes': ["off"],
+            '@stylistic/semi': ["error", "always"],
+            "@stylistic/spaced-comment": ["off"],
+        }
     }
-  }
 ]);
